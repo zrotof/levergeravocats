81dc8561fcbdf02aa1991b29527094b268e91aa5
Zrotof <manduel21@gmail.com>
2021-03-08 12:02:28 +0100

    update to deploy on heroku server 3

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/NguepiBarberShopFrontEnd'));
+app.use(express.static(__dirname + '/dist/nbsfe'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/NguepiBarberShopFrontEnd/index.html'));
+res.sendFile(path.join(__dirname+'/dist/nbsfe/index.html'));
 });
 
 
